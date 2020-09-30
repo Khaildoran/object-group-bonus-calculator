3230036d8bbbf9a0cfbe940482911f9241214d82
Elliot Malcolm <talcumalc@gmail.com>
2020-09-30 10:32:14 -0500

    complete required goals

--- client.js
+++ client.js
@@ -44,53 +44,96 @@ console.log( employees );
 
 // this function should contain the bonus percentage the employee is to receive.
 function bonusCalc(employee) {
+  
   let bonusPercent = 0;
   let employeeNumber = employee.employeeNumber;
+  console.log(employeeNumber);
   let reviewScore = employee.reviewRating;
+  console.log(reviewScore);
   let annualIncome = employee.annualSalary;
+  console.log(annualIncome);
 
-  function ratingCalc(reviewScore){
+  function ratingCalc(reviewScore)
+  {
     if( reviewScore <= 2 ){
     bonusPercent = 1
     }
-    else if ( reviewScore = 3){
+    else if (reviewScore === 3){
     bonusPercent = 1.04
     }
-    else if ( reviewScore = 4){
+    else if ( reviewScore === 4){
     bonusPercent = 1.06
     }
-    else if ( reviewScore = 5){
+    else if ( reviewScore === 5){
     bonusPercent = 1.10
     }
   }
-  function employNumberCalc(employeeNumber){
+
+  function employNumberCalc(employeeNumber)
+  {
     if(employeeNumber.length === 4 )
     {
-    bonusPercent += .05
+      bonusPercent += .05
     }
   }
 
-  function annualIncomeCalc(annualIncome){
-    if()
+  function annualIncomeCalc(annualIncome)
+  {
+    if(annualIncome > 65000 )
+    {
+      bonusPercent -= .01
+    }
   }
 
-  function maxMinBonusGate(bonusPercent){
-    
+  function maxMinBonusGate(totalPercent)
+  {
+    if(totalPercent < 1)
+    {
+      bonusPercent = 1
+    }
+    else if(totalPercent > 1.13) 
+    {
+      bonusPercent = 1.13
+    };
   }
   
+  
+  ratingCalc(reviewScore);
+  
+  employNumberCalc(employeeNumber);
+  
+  annualIncomeCalc(annualIncome);
+  
+  maxMinBonusGate(bonusPercent);
+  
+
+  return bonusPercent;
 }
+
+console.log(bonusCalc(employees[0]));
+console.log(bonusCalc(employees[1]));
+console.log(bonusCalc(employees[2]));
+
 
 // this function should be the adjusted annual compensation (base annual + bonus)
-function totalCompCalc(params) {
+// function totalCompCalc(params) {
   
+function totalCompCalc(employee, calcFunction){
+  let employeeIncome = employee.annualSalary
+  let employeeBonusMult = calcFunction(employee)
+  let totalComp = parseInt(employeeBonusMult * employeeIncome)
+  return totalComp
 }
+console.log(totalCompCalc(employees[2], bonusCalc));
 
-// this function should be the employee's total bonus roundered to the nearest dollar
-function totalBonusCalc(params) {
-  
-}
+// function BonusMetrics(employee){
+//   this.name = employee.name
+//   this.bonusPercentage = bunusCalc(employee)
+// }
 
-function BonusMetrics(employee){
-  this.name = employee.name
-  this.bonusPercentage = bunusCalc(employee)
+function totalBonus(employee, bonusCalc){
+  let bonusPercent = (bonusCalc(employee) - 1)
+  let totalBonus = bonusPercent * employee.annualSalary 
+  return totalBonus 
 }
+console.log(totalBonus(employees[2], bonusCalc));
